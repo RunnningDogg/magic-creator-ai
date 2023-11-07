7894b484627526f95cbc3e478c170507535925fe
liangjiongxin <liangjiongxin@baidu.com>
2023-11-07 20:45:48 +0800

    feature vercel analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 // const inter = Inter({ subsets: ["latin"] });
 
 import { Inter as FontSans } from "next/font/google";
@@ -34,6 +34,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
