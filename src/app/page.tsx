72d6a652ee6b8ad48c879ef0fa9d4731320be1c9
liangjiongxin <liangjiongxin@baidu.com>
2023-11-15 22:35:09 +0800

    feature: fix page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,19 +34,8 @@ export default function Home() {
 
       <main className="flex w-full flex-col items-center justify-center ">
         {/* py-[10vh] sm:py-[5vh] */}
-        <div className="mb-3 mt-5">
-          <Alert className="w-xl">
-            {/* <Terminal className="h-4 w-4" /> */}
-            <AlertTitle>Heads up!</AlertTitle>
-            <AlertDescription>
-              <p className="font-bold">
-                Vote for your favorite GPTs in content page
-              </p>
-            </AlertDescription>
-          </Alert>
-        </div>
 
-        <h1 className="mb-3 text-4xl font-bold text-black duration-1000 ease-in-out animate-in fade-in slide-in-from-bottom-3">
+        <h1 className="mb-3 mt-10 text-4xl font-bold text-black duration-1000 ease-in-out animate-in fade-in slide-in-from-bottom-3">
           GPTs Store{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
@@ -76,19 +65,30 @@ export default function Home() {
           </span>{" "}
         </h1>
 
-        <a
-          href="https://www.producthunt.com/posts/gpts-store?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-gpts&#0045;store"
-          target="_blank"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=425391&theme=light"
-            alt="GPTs&#0045;store - Explore&#0044;&#0032;Vote&#0044;&#0032;and&#0032;Discuss&#0058;&#0032;explore&#0032;the&#0032;World&#0032;of&#0032;GPTs | Product Hunt"
-            // style="width: 250px; height: 54px;"
-            className="h-[54px] w-[250px]"
-            width="250"
-            height="54"
-          />
-        </a>
+        <div className="flex gap-3">
+          <a
+            href="https://www.producthunt.com/posts/gpts-store?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-gpts&#0045;store"
+            target="_blank"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=425391&theme=light"
+              alt="GPTs&#0045;store - Explore&#0044;&#0032;Vote&#0044;&#0032;and&#0032;Discuss&#0058;&#0032;explore&#0032;the&#0032;World&#0032;of&#0032;GPTs | Product Hunt"
+              // style="width: 250px; height: 54px;"
+              className="h-[54px] w-[250px]"
+              width="250"
+              height="54"
+            />
+          </a>
+          {/* <Alert className="w-xl">
+            
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>
+              <p className="font-bold">
+                Vote for your favorite GPTs in content page
+              </p>
+            </AlertDescription>
+          </Alert> */}
+        </div>
         <div className="mt-3 min-h-[50vh]">
           <TagsListRemote />
         </div>
